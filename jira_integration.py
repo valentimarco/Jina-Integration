4a61e1d16eaf1b901cc8e56ba6da700c4acf97ec
marco <gamespy67@gmail.com>
2024-01-15 09:20:36 +0100

    Version 0.0.1

---
+++ jira_integration.py
@@ -0,0 +1,26 @@
+from typing import Type, List
+
+from cat.mad_hatter.decorators import tool, hook, plugin
+from cat.factory.embedder import EmbedderSettings
+from langchain.embeddings import JinaEmbeddings
+from pydantic import ConfigDict
+
+
+class JinaEmbedderConfig(EmbedderSettings):
+    jina_api_key: str
+    model_name: str='jina-embeddings-v2-base-en'
+    _pyclass: Type = JinaEmbeddings
+
+    model_config = ConfigDict(
+        json_schema_extra = {
+            "humanReadableName": "Jina embedder",
+            "description": "Jina embedder",
+            "link": "https://jina.ai/embeddings/",
+        }
+    )
+
+
+@hook
+def factory_allowed_embedders(allowed, cat) -> List:
+    allowed.append(JinaEmbedderConfig)
+    return allowed
